0ecd247a2ed7cc6c3330c6bc7612b7cc6283587d
Phil Fernandez <norcal618@live.com>
2016-06-28 16:31:02 -0700

    doit

--- MyCode/js/app.js
+++ MyCode/js/app.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
     var clientId = '1032306829714-7d5q6lrv7b4ditg4pt43q8004f52prjk.apps.googleusercontent.com';
     var scopes =
-        'https://www.googleapis.com/auth/gmail.readonly ' +
-        'https://www.googleapis.com/auth/gmail.send';
+        ['https://www.googleapis.com/auth/gmail.readonly ' +
+        'https://www.googleapis.com/auth/gmail.send'];
 
-        //left out handleClientLoad() apiKey
+        
 
         function checkAuth() {
-            gapi.auth.authorize({
+            gapi.auth.authorize(
+            {
                 client_id: clientId,
-                scope: scopes,
+                scope: scopes.join(' '),
                 immediate: true
             }, handleAuthResult);
         }
 
-        function handleAuthClick() {
+
+
+        function handleAuthResult(authResult) {
+            var authorizeDiv = document.getElementById('authorize-div');
+            if (authResult && !authResult.error) {
+                authorizeDiv.style.display = 'none';
+                loadGmailApi();
+            }
+            else {
+                authorizeDiv.style.display = 'inline';
+            }          
+        }
+
+        function handleAuthClick(event) {
             gapi.auth.authorize({
                 client_id: clientId,
                 scope: scopes,
@@ -23,27 +37,32 @@ $(document).ready(function() {
             return false;
         }
 
-        function handleAuthResult(authResult) {
-            if(authResult && !authResult.error) {
-                loadGmailApi();
-                $('#authorize-button').remove();
-                $('.table-inbox').removeClass('hidden');
-                $('#compose-button').removeClass('hidden');                
-            } else {
-                $('#authorize-button').removeClass('hidden');
-                $('#authorize-button').on('click', function() {
-                    handleAuthClick();
-                });
-            }
-        }
-
         function loadGmailApi() {
             gapi.client.load('gmail', 'v1', displayInbox);
         }
 
         function displayInbox() {
-            var request = gapi.client.gmail.users.message
+            var request = gapi.client.gmail.users.messages.list({
+                'userId': 'me',
+                'labelIds' : 'INBOX',
+                'maxResults' : 10
+            });
+            request.execute(function(resp) {
+                $.each(resp.messages, function() {
+                    var messageRequest = gapi.client.gmail.users.messages.get({
+                        'userId': 'me',
+                        'id': this.id
+                    });
+                    messageRequest.execute(appendMessageRow);
+                });
+            });
         }
+
+        function appendMessageRow(message) {
+            $('#mail').append(message.id);
+        }
+                      
+        
 
 });
 
